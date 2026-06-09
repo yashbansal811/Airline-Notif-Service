8460bd44b2b02881dbec05409011352d8ce3dca9
Yash <yashb067@gmail.com>
2026-06-09 19:51:48 +0530

    Completed consumption and sending mail

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 const express = require('express');
+const amqplib=require('amqplib');
+const{EmailService}=require('./services')
+
+async function connectQueue(){
+    try{
+        const connection=await amqplib.connect("amqp://localhost");
+        const channel=await connection.createChannel();
+        await channel.assertQueue("noti-queue");
+        channel.consume("noti-queue",async (data)=>{
+            const object= JSON.parse(`${Buffer.from(data.content)}`);
+            await EmailService.sendEmail("yashbansal811@gmail.com",object.recipientEmail,object.subject,object.text);
+            channel.ack(data);
+        })
+    }
+    catch(error){
+
+    }
+}
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
@@ -10,6 +28,8 @@ app.use(express.urlencoded({extended:true}));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    await connectQueue();
+    console.log("queue is up"); 
 });
